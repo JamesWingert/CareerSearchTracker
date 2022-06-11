2b813115828c0917b7a9a76f8017a03769b4ca95
James Wingert <jameswingert1@gmail.com>
2022-06-10 23:22:02 -0700

    added linting

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,20 +1,20 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms'), require('daisyui')],
+  plugins: [require("@tailwindcss/forms"), require("daisyui")],
   daisyui: {
     styled: true,
     themes: [
       {
         winter: {
-          ...require('daisyui/src/colors/themes')['[data-theme=winter]'],
-          warning: '#548CA8',
+          ...require("daisyui/src/colors/themes")["[data-theme=winter]"],
+          warning: "#548CA8",
         },
         dracula: {
-          ...require('daisyui/src/colors/themes')['[data-theme=dracula]'],
-          primary: '#f6f8fa',
+          ...require("daisyui/src/colors/themes")["[data-theme=dracula]"],
+          primary: "#f6f8fa",
         },
       },
     ],
@@ -23,7 +23,7 @@ module.exports = {
     utils: true,
     logs: true,
     rtl: false,
-    prefix: '',
-    darkTheme: 'dracula',
+    prefix: "",
+    darkTheme: "dracula",
   },
 };
