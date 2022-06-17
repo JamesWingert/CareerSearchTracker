a53c3e3b61520053702ae51ebf927622afe444fb
James Wingert <jameswingert1@gmail.com>
2022-06-16 20:33:23 -0700

    Changed styles

--- client/src/components/Alert.jsx
+++ client/src/components/Alert.jsx
@@ -2,5 +2,5 @@ import { useAppContext } from "../context/appContext";
 
 export default function Alert() {
   const { alertType, alertText } = useAppContext();
-  return <div className={`alert alert-${alertType}`}>{alertText}</div>;
+  return <div className={`bg-base-100 text-neutral alert alert-${alertType}`}>{alertText}</div>;
 }
