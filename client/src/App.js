744f4493434a718b30a99056556f444106b2006a
James Wingert <jameswingert1@gmail.com>
2022-06-21 06:41:42 -0700

    Fixed styling

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,6 @@ function App() {
           <Route index element={<Stats />} />
           <Route path="all-jobs" element={<AllJobs />} />
           <Route path="add-job" element={<AddJob />} />
-          {/* <Route path="profile" element={<Profile />} /> */}
         </Route>
 
         <Route path="/landing" element={<Landing />} />
